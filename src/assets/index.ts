3c9d2dd94d3e3d6fd9bad42ac972b1f358611cfa
rafamoliv <rafaoliv.ti@gmail.com>
2023-04-06 16:26:13 -0300

    feat: insert assets and animations

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Icons
+ */
+
+export { default as IconPokeGGBorderWhite } from './poke.gg-border_white.svg'
+export { default as IconPokeGG } from './poke.gg.svg'
+export { default as IconPokeGGSmall } from './gg.svg'
+
+/**
+ * Animations
+ */
+
+export { default as AnimationLoader } from './animations/loader-animation.json'
