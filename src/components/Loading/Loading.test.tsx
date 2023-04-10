62459c8f651901f652e83aa97d0c074b28cb2f13
rafamoliv <rafaoliv.ti@gmail.com>
2023-04-10 16:47:03 -0300

    feat: test all prop cases 100% coverage

--- src/components/Loading/Loading.test.tsx
+++ src/components/Loading/Loading.test.tsx
@@ -18,27 +18,28 @@ describe('<Loading />', () => {
   describe('Component Props', () => {
     it('Should render children props', () => {
       const { container } = render(<Loading loading={false} children={'foo'} />)
-      expect(screen.getByText(/foo/i)).toBeTruthy()
-      expect(container.firstChild).toBeTruthy()
-    })
-
-    it('Should render default loading', () => {
-      const { container } = render(<Loading size={undefined} {...props} />)
+      expect(screen.getByText(/foo/i)).toBeInTheDocument()
       expect(container.firstChild).toBeTruthy()
     })
 
     it('Should render small loading', () => {
       const { container } = render(<Loading size='sm' {...props} />)
+      expect(screen.getByRole(/button/i)).toHaveStyle('width: 32px')
+      expect(screen.getByRole(/button/i)).toBeInTheDocument()
       expect(container.firstChild).toBeTruthy()
     })
 
     it('Should render medium loading', () => {
       const { container } = render(<Loading size='md' {...props} />)
+      expect(screen.getByRole(/button/i)).toHaveStyle('width: 68px')
+      expect(screen.getByRole(/button/i)).toBeInTheDocument()
       expect(container.firstChild).toBeTruthy()
     })
 
     it('Should render big loading', () => {
       const { container } = render(<Loading size='bg' {...props} />)
+      expect(screen.getByRole(/button/i)).toHaveStyle('width: 125px')
+      expect(screen.getByRole(/button/i)).toBeInTheDocument()
       expect(container.firstChild).toBeTruthy()
     })
   })
