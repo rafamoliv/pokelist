6b7cbaeed0ba764c6ebb323d3cb8d3aa86ba303e
rafamoliv <rafaoliv.ti@gmail.com>
2023-04-10 19:23:55 -0300

    feat: begin home page tests

---
+++ src/pages/Home/Home.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen, userEvent } from '@/utils/tests'
+import Home from './Home'
+
+describe('<Home />', () => {
+  describe('Component Snapshot', () => {
+    it('Render element', () => {
+      const { container } = render(<Home />)
+      expect(container.firstChild).toMatchSnapshot()
+    })
+  })
+
+  describe('Component Roles', () => {
+    render(<Home />)
+
+    it('Should have title', () => {
+      render(<Home />)
+      expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument()
+    })
+
+    it('Should have button', () => {
+      render(<Home />)
+      expect(screen.getAllByRole(/button/i)[0]).toBeInTheDocument()
+    })
+
+    it('Should have text', () => {
+      render(<Home />)
+      expect(screen.getAllByRole(/p/i)[0]).toBeInTheDocument()
+    })
+  })
+})
