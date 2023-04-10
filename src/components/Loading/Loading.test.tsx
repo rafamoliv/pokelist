a1ce45fb7b2001dc8e725d3d7599d68fc07c5ad3
rafamoliv <rafaoliv.ti@gmail.com>
2023-04-10 14:36:19 -0300

    feat: begin loading component test

--- src/components/Loading/Loading.test.tsx
+++ src/components/Loading/Loading.test.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
 import i18next from 'i18next'
-import { expect, it } from 'vitest'
-
-import { render } from '../../utils/tests'
-
+import { render, screen } from '@/utils/tests'
 import { Loading } from '.'
 
 const props = {
@@ -13,9 +9,37 @@ const props = {
 
 describe('<Loading />', () => {
   describe('Component Snapshot', () => {
-    it('render element', () => {
+    it('Render element', () => {
       const { container } = render(<Loading {...props} />)
       expect(container.firstChild).toMatchSnapshot()
+    })
+  })
+
+  describe('Component Props', () => {
+    it('Should render children props', () => {
+      const { container } = render(<Loading loading={false} children={'foo'} />)
+      expect(screen.getByText(/foo/i)).toBeTruthy()
+      expect(container.firstChild).toBeTruthy()
+    })
+
+    it('Should render default loading', () => {
+      const { container } = render(<Loading size={undefined} {...props} />)
+      expect(container.firstChild).toBeTruthy()
+    })
+
+    it('Should render small loading', () => {
+      const { container } = render(<Loading size='sm' {...props} />)
+      expect(container.firstChild).toBeTruthy()
+    })
+
+    it('Should render medium loading', () => {
+      const { container } = render(<Loading size='md' {...props} />)
+      expect(container.firstChild).toBeTruthy()
+    })
+
+    it('Should render big loading', () => {
+      const { container } = render(<Loading size='bg' {...props} />)
+      expect(container.firstChild).toBeTruthy()
     })
   })
 })
